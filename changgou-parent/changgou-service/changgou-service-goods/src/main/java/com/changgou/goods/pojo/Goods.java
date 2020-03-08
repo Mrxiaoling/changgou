3ea4f4e99da2dc43e4459a6dd1bbe67ccfe94955
“Mrxiaoling <ling1471626451@gmail.com>
2020-03-08 22:46:33 +0800

    第三天

---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/pojo/Goods.java
@@ -0,0 +1,29 @@
+package com.changgou.goods.pojo;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 商品信息组合对象
+ *
+ */
+public class Goods implements Serializable {
+   private Spu spu;
+   private List<Sku> skus;
+
+    public Spu getSpu() {
+        return spu;
+    }
+
+    public void setSpu(Spu spu) {
+        this.spu = spu;
+    }
+
+    public List<Sku> getSkus() {
+        return skus;
+    }
+
+    public void setSkus(List<Sku> skus) {
+        this.skus = skus;
+    }
+}
